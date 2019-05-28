051df9809099a29019be7574802a2c8863c4c155
Damian Suess <djsuess@gmail.com>
2019-05-28 18:43:06 -0400

    base tests

---
+++ Test.CryptoCoins.Client/Program.cs
@@ -0,0 +1,28 @@
+﻿using NBitcoin;
+using System;
+
+namespace Test.CryptoCoins.Client
+{
+  internal static class Program
+  {
+    /// <summary>
+    /// The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    private static void Main()
+    {
+      ////Application.EnableVisualStyles();
+      ////Application.SetCompatibleTextRenderingDefault(false);
+      ////Application.Run(new Form1());
+      ///
+
+      CoinTests();
+    }
+
+    private static void CoinTests()
+    { 
+      // Wif(Network.Main): "L5YBgwdqKv3C5MUk5BHBCGQpwkEGb2PDuQhxu4GJ1btkLiZB71Be"
+      Console.WriteLine("Hello : " + new Key().GetWif(Network.Main));
+    }
+  }
+}
